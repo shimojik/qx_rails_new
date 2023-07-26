402566277672eef981bf9ba6cfd656035a20ac70
Kiyoshi Mikuni <houdig@gmail.com>
2023-07-26 12:06:33 +0900

    Import mapの動作確認

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import { myFunction } from "my_module";
+
+document.addEventListener("DOMContentLoaded", () => {
+    myFunction();
+});
---
+++ app/javascript/my_module.js
@@ -0,0 +1,3 @@
+export function myFunction() {
+  console.log("Hello from my_module.js! This is a function in a module.");
+}
