9bbfbdf914ea35ef023f3e729f21bdd055944031
Kenta Shimojima <curacher@gmail.com>
2024-09-16 13:06:38 +0900

    content_generatorの動作確認

--- app/services/ai/content_generator.rb
+++ app/services/ai/content_generator.rb
@@ -17,7 +17,6 @@ module Ai
         api_key: ENV['ANTHROPIC_API_KEY'],
         default_options: {
           chat_completion_model_name: 'claude-3-sonnet-20240229',
-          max_tokens_to_sample: params[:max_length].to_i,
           temperature: 0.7
         }
       )
@@ -26,6 +25,7 @@ module Ai
         あなたは優秀なコンテンツ作成者です。与えられた文章を元に、新しいコンテンツを生成してください。
         元の意味を保持しつつ、創造的で興味深いコンテンツを作成することが目標です。
         最大文字数は#{params[:max_length]}文字です。
+        * jsonや```のような記号を含め前後に何も入れず直接結果のみを出力してください
 
         文章：
         {text}
@@ -42,10 +42,10 @@ module Ai
       assistant.add_message_and_run(content: params[:body])
 
       messages = assistant.messages
-      generated_content = messages.last.content
+      content = messages.last.content
 
       {
-        body: generated_content,
+        body: content,
       }
     end
   end
